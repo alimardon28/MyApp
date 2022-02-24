9c0a1138e25e6039202e4f39ac6083355ca81868
alimardon28 <doniyorovalimardon369@gmail.com>
2022-02-24 23:06:35 +0500

    added function website fineshed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,76 @@
 import './App.css';
+import { useState } from 'react';
+import data from "./Components/localization/content";
+import notImg from './assets/img/img.png';
+import not1Img from './assets/img/img1.jpg';
+import not2Img from './assets/img/img2.jpg';
+import not3Img from './assets/img/img3.png';
+import not4Img from './assets/img/img5.jpg';
+
 
 function App() {
-  return (
-    <div className="App">
-        <p>hello</p>
-    </div>
+  const [lang , setLang] = useState('uz');
+      return (
+          <>
+            <div className='Language__box'>
+             <header className='Header'>
+            <div className="container">
+                <nav className='Navbar'>
+                    <a className='Navbar__logo' href="#">{data.header[lang].content}</a>
+                    <ul className='Navbar__list'>
+                        <li className='Navbar__item'><a href="#">{data.nav[lang].content}</a></li>
+                        <li className='Navbar__item'><a href="#">{data.nav[lang].content}</a></li>
+                        <li className='Navbar__item'><a href="#">{data.nav[lang].content}</a></li>
+                       <select defaultValue={setLang}  onChange={(e) => setLang(e.target.value)} className='Select'>
+                           <option  value="uz">UZB</option>
+                           <option  value="eng">ENG</option>
+                           <option  value="ru">RUS</option>
+                       </select>
+                    </ul>
+                </nav>
+             </div>
+             </header>
+           <main className='Main'>
+             <section className='Section__hero'>
+               <div className="container">
+                   <div className="NoutbookBox">
+                       <p  className='NoutbookBox__desc'>{data.Section__hero[lang].content}</p>
+                       <img className='NoutbookBox__img' src={notImg} alt="img" width={500} height={400} />
+                   </div>
+               </div>
+             </section>
+             <section className='Section__top'>
+                <div className='container'>
+                  <div className="NoutbookHeading">
+                   <h2>{data.Section__top.NoutbookHeading[lang].content}</h2>
+                   <div className="NoutbookServices">
+                       <div className="dellBox">
+                       <img src={not1Img} alt="img" />
+                       <p>{data.Section__top.dellBox[lang].content}</p>
+                       </div>
+                       <div className="acerBox">
+                       <img src={not2Img} alt="img" />
+                       <p>{data.Section__top.acerBox[lang].content}</p>
+                       </div>
+                        <div className="hpBox">
+                          <img src={not3Img} alt="img" />
+                          <p>{data.Section__top.hpBox[lang].content}</p>
+                        </div>
+                   </div>
+                   </div>
+                  </div>
+             </section>
+             <section className='Section__bottom'>
+               <div className='container'>
+                   <div className="NoutbookPage">
+                       <img src={not4Img} alt="img" width={600} height={400} />
+                       <p>{data.Section__bottom[lang].content}</p>
+                   </div>
+               </div>
+             </section>
+           </main>
+         </div>
+      </>
   );
 }
 
---
+++ src/Components/localization/content.jsx
@@ -0,0 +1,113 @@
+const data = {
+
+    header: {
+            ru:{
+                content:"Рус"
+
+             },
+             uz:{
+                 content:"O'bek"
+             },
+             eng:{
+                 content:"English"
+             }
+         },
+
+    nav:{
+                ru:{
+                    content:"Дома"
+
+                 },
+                 uz:{
+                     content:"Uy"
+                 },
+                 eng:{
+                     content:"Home"
+                 }
+        },
+
+    Section__hero:{
+          ru:{
+             content:"Нетбуки как отдельная категория ноутбуков были выделены из категории субноутбуков в первом квартале 2008 года компанией Intel[1]. Размер диагонали нетбуков от 7 до 12,1 дюймов. Нетбуки ориентированы на просмотр веб-страниц, работу с электронной почтой и офисными программами. Для этих ноутбуков разработаны специальные энергоэффективные процессоры Intel Atom, VIA C7, VIA Nano, AMD Geode. Небольшой размер экрана, небольшая клавиатура и невысокая производительность таких устройств компенсируются умеренной ценой и относительно длительным временем автономной работы. Габариты обычно не позволяют установить в нетбук оптический привод, а вот адаптер Wi-Fi — обязательный компонент."
+          },
+          uz:{
+            content:"Netbuklar alohida toifadagi noutbuklar sifatida 2008 yilning birinchi choragida Intel tomonidan subnotebooklar toifasidan ajratilgan[1]. Netbuklarning diagonali o'lchami 7 dan 12,1 dyuymgacha. Netbuklar internetni ko'rishga, elektron pochta va ofis dasturlari bilan ishlashga qaratilgan. Ushbu noutbuklar uchun maxsus energiya tejovchi Intel Atom, VIA C7, VIA Nano, AMD Geode protsessorlari ishlab chiqilgan. Kichkina ekran o'lchami, kichik klaviatura va bunday qurilmalarning past ishlashi o'rtacha narx va nisbatan uzoq batareya muddati bilan qoplanadi. O'lchovlar odatda netbukda optik diskni o'rnatishga imkon bermaydi, lekin Wi-Fi adapteri zarur komponent hisoblanadi."
+        },
+          eng:{
+            content:"Netbooks as a separate category of notebooks were separated from the category of subnotebooks in the first quarter of 2008 by Intel[1]. Diagonal size of netbooks from 7 to 12.1 inches. Netbooks are focused on browsing the web, working with e-mail and office programs. Special power-efficient processors Intel Atom, VIA C7, VIA Nano, AMD Geode have been developed for these notebooks. The small screen size, small keyboard and low performance of such devices is offset by a moderate price and relatively long battery life. Dimensions usually do not allow you to install an optical drive in a netbook, but a Wi-Fi adapter is a required component."
+        }
+        },
+
+    Section__top:{
+
+    NoutbookHeading:{
+        ru:{
+            content:"Типы ноутбуков."
+
+         },
+         uz:{
+             content:"Noutbuk turlari."
+         },
+         eng:{
+             content:"Types of laptops."
+         }
+        },
+
+        dellBox:{
+
+
+        ru:{
+           content:"Ноутбук Делл.",
+
+          },
+        uz:{
+          content:"Dell noutbuk."
+           },
+        eng:{
+          content:"Dell laptop."
+           }
+            },
+        acerBox:{
+
+
+        ru:{
+               content:"Ноутбук Асер.",
+
+              },
+        uz:{
+              content:"Acer noutbuk."
+               },
+        eng:{
+              content:"Acer Laptop."
+                }
+            },
+        hpBox:{
+
+
+        ru:{
+            content:"HP Ноутбук.",
+
+                },
+         uz:{
+            content:"HP noutbuk."
+                },
+        eng:{
+            content:"HP laptop."
+                }
+            },
+        },
+
+    Section__bottom:{
+        ru:{
+            content:"Субноутбуки — это ноутбуки с диагональю экрана 11-13,3 дюйма. Эти ноутбуки имеют небольшие размеры и вес, но небольшой размер экрана снижает удобство использования такого устройства. Субноутбуки недостаточно велики для размещения мощных компонентов из-за проблем с охлаждением, поэтому в них часто используются мобильные процессоры с низким энергопотреблением (модели LV или ULV). Субноутбуки редко оснащаются дискретными видеокартами, а в некоторых моделях не установлен привод оптических дисков."
+         },
+         uz:{
+           content:"Subnotebooklar ekrani 11-13,3 dyuymli noutbuklardir. Ushbu noutbuklar hajmi va vazni kichik, ammo kichik ekran o'lchami bunday qurilmadan foydalanish imkoniyatini pasaytiradi. Subnotebooklar sovutish muammolari tufayli kuchli komponentlarni joylashtirish uchun etarlicha katta emas, shuning uchun ular ko'pincha kam quvvatli mobil protsessorlardan foydalanadilar (LV yoki ULV modellari). Subnotebooklar kamdan-kam hollarda diskret grafik kartalar bilan jihozlangan va ba'zi modellarda optik disk o'rnatilmagan."
+       },
+         eng:{
+           content:"Subnotebooks are laptops with a screen size of 11-13.3 inches. These laptops are small in size and weight, but the small screen size reduces the usability of such a device. Subnotebooks are not large enough to accommodate powerful components due to cooling issues, so they often use low power mobile processors (LV or ULV models). Subnotebooks are rarely equipped with discrete graphics cards, and some models do not have an optical drive installed.."
+       }
+    }
+}
+
+export default data;
